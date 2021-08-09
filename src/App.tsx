9e71b6a7bb7098e59f4ad2a2ca4333a3fc28618b
Piyush Mehta <me@piyushmehta.com>
2021-08-09 20:39:30 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+function App() {
+  return (
+    <div className="App">
+      <Helmet>
+        <html data-theme="aqua" />
+      </Helmet>
+    </div>
+  );
+}
+
+export default App;

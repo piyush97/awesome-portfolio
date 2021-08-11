0480d467792c93409d1e603e8c509c586606453e
Piyush Mehta <me@piyushmehta.com>
2021-08-11 23:17:47 +0530

    SEO Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
+import Seo from "./components/Seo";
 import HomeContainer from "./containers/HomeContainer";
 import { ThemeContext, ThemeContextProps } from "./context/ThemeProvider";
+import { GREETING_DESCRIPTION, IMAGE, KEYWORDS, NAME } from "./data/data";
 
 function App() {
   const [Theme, setTheme] = useState("cupcake");
@@ -9,6 +11,14 @@ function App() {
   return (
     <ThemeContext.Provider value={value}>
       <HomeContainer />
+      <Seo
+        lang="en"
+        metaDescription={GREETING_DESCRIPTION}
+        keywords={KEYWORDS}
+        image={IMAGE}
+        title={NAME}
+        author={NAME}
+      />
       <Helmet>
         <html data-theme={Theme} />
       </Helmet>
---
+++ src/components/Seo.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { SEOProps } from "../data/data";
+
+const Seo: React.FC<SEOProps> = ({
+  lang,
+  metaDescription,
+  keywords,
+  title,
+  author,
+  image: metaImage,
+}) => {
+  const image = metaImage && metaImage.src ? `${metaImage.src}` : null;
+  return (
+    <Helmet
+      htmlAttributes={{ lang }}
+      meta={[
+        { name: `description`, content: metaDescription },
+        { name: `keywords`, content: keywords.join(",") },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:creator`,
+          content: author,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+        {
+          property: "og:image",
+          content: image,
+        },
+        {
+          property: "og:image:width",
+          content: metaImage.width,
+        },
+        {
+          property: "og:image:height",
+          content: metaImage.height,
+        },
+        {
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+      ]}
+      title={title}
+      titleTemplate={`%s | ${title}`}
+    />
+  );
+};
+
+export default Seo;

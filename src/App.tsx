de2d84870bd53fce98461044d089fd4fdb2243fc
Piyush Mehta <me@piyushmehta.com>
2021-08-11 23:44:22 +0530

    maybe

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
-import Seo from "./components/Seo";
 import HomeContainer from "./containers/HomeContainer";
 import { ThemeContext, ThemeContextProps } from "./context/ThemeProvider";
-import { GREETING_DESCRIPTION, IMAGE, KEYWORDS, NAME } from "./data/data";
 
 function App() {
   const [Theme, setTheme] = useState("cupcake");
@@ -11,14 +9,6 @@ function App() {
   return (
     <ThemeContext.Provider value={value}>
       <HomeContainer />
-      <Seo
-        lang="en"
-        metaDescription={GREETING_DESCRIPTION}
-        keywords={KEYWORDS}
-        image={IMAGE}
-        title={NAME}
-        author={NAME}
-      />
       <Helmet>
         <html data-theme={Theme} />
       </Helmet>
--- src/containers/HomeContainer.tsx
+++ src/containers/HomeContainer.tsx
@@ -3,6 +3,8 @@ import Drawer from "../components/Drawer";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Seo from "../components/Seo";
+import { GREETING_DESCRIPTION, IMAGE, KEYWORDS, NAME } from "../data/data";
 import ExperienceContainer from "./ExperienceContainer";
 
 const HomeContainer: React.FC = () => {
@@ -10,6 +12,14 @@ const HomeContainer: React.FC = () => {
 
   return (
     <>
+      <Seo
+        lang="en"
+        metaDescription={GREETING_DESCRIPTION}
+        keywords={KEYWORDS}
+        image={IMAGE}
+        title={NAME}
+        author={NAME}
+      />
       <Navbar menuShow={menuShow} showMenu={showMenu} />
       {menuShow && <Drawer menuShow />}
       <Hero />

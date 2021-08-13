31cbc86473a17e2eb6948eb2b664adab23565795
Piyush Mehta <me@piyushmehta.com>
2021-08-13 23:09:57 +0530

    multiple theming fixes

--- src/containers/SkillsContainer.tsx
+++ src/containers/SkillsContainer.tsx
@@ -8,9 +8,22 @@ const SkillsContainer: React.FC = () => {
       <Heading heading={SECTIONS[3]} id="skills" />
       <div className="grid gap-6 mt-5 lg:grid-cols-4 justify-items-center auto-rows-fr">
         {skills.map(({ id, skill, level }) => (
-          <div className="p-6 space-y-2 artboard phone">
+          <div
+            key={id}
+            className={`p-6 space-y-2 artboard phone card ${
+              id % 2 === 0
+                ? `bg-secondary text-secondary-content`
+                : `bg-primary text-primary-content`
+            }`}
+          >
             <h3>{skill}</h3>
-            <progress className="progress" value={level} max="100" />
+            <progress
+              className={`progress ${
+                id % 2 === 0 ? `progress-primary` : `progress-secondary`
+              }`}
+              value={level}
+              max="100"
+            />
           </div>
         ))}
       </div>

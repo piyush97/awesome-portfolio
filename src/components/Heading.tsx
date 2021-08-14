df378339544df33dbdbecbe7f10e61a9d566a902
Piyush Mehta <me@piyushmehta.com>
2021-08-14 13:58:52 +0530

    setting

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,11 +3,11 @@ import { HeadingProps } from "../types/types";
 
 const Heading: React.FC<HeadingProps> = ({ heading, id, illustration }) => {
   return (
-    <div className="grid my-2 mt-2 center lg:grid-cols-2 auto-rows-fr">
+    <div className="grid my-2 mt-2 center lg:grid-cols-2 auto-rows-fr ">
       <div className="hidden pt-3 antialiased lg:flex" id={id}>
         {illustration}
       </div>
-      <div className="pt-10 text-7xl md:text-center">{heading}</div>
+      <div className="pt-10 ml-10 text-7xl md:text-center">{heading}</div>
     </div>
   );
 };

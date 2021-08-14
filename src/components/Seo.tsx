93800436358d6035b842adce64a1674efe02870e
Piyush Mehta <me@piyushmehta.com>
2021-08-14 23:09:21 +0530

    fixes

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -16,14 +16,26 @@ const Seo: React.FC<SEOProps> = ({
   // const image = metaImage && metaImage.src ? `${metaImage.src}` : null;
   return (
     <Helmet>
-      <html data-theme={theme} />
+      <html data-theme={theme} data-react-helmet="true" />
       <title>{title}</title>
-      <meta name="title" content={title} />
-      <meta name="description" content={metaDescription} />
-      <meta property="og:title" content={title} />
-      <meta property="og:image" content={metaImage.src} />
-      <meta property="og:description" content={metaDescription} />
-      <meta property="og:url" content={URL} />
+      <meta name="title" content={title} data-react-helmet="true" />
+      <meta
+        name="description"
+        content={metaDescription}
+        data-react-helmet="true"
+      />
+      <meta property="og:title" content={title} data-react-helmet="true" />
+      <meta
+        property="og:image"
+        content={metaImage.src}
+        data-react-helmet="true"
+      />
+      <meta
+        property="og:description"
+        content={metaDescription}
+        data-react-helmet="true"
+      />
+      <meta property="og:url" content={URL} data-react-helmet="true" />
     </Helmet>
   );
 };

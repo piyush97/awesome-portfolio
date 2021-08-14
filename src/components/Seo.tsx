085383d49ab8b6031b9ae40932144445441b84e1
Piyush Mehta <me@piyushmehta.com>
2021-08-14 23:01:25 +0530

    fixing seo

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { URL } from "../data/data";
 import { SEOProps } from "../types/types";
 
 const Seo: React.FC<SEOProps> = ({
@@ -14,14 +15,16 @@ const Seo: React.FC<SEOProps> = ({
 }) => {
   // const image = metaImage && metaImage.src ? `${metaImage.src}` : null;
   return (
-    <Helmet
-      htmlAttributes={{ lang }}
-      meta={[
-        { name: `description`, content: metaDescription },
-        { name: `keywords`, content: keywords.join(",") },
-      ]}
-    >
+    <Helmet htmlAttributes={{ lang }}>
       <html data-theme={theme} />
+      <title>{title}</title>
+
+      <meta name="title" content={title} />
+      <meta name="description" content={metaDescription} />
+      <meta property="og:title" content={title} />
+      <meta property="og:image" content={metaImage.src} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:url" content={URL} />
     </Helmet>
   );
 };
